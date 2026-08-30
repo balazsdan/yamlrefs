448875f6f086db70d0ae0c434ea986b0a9c94759
balazsdan <balazs.dan@gmail.com>
2026-08-30 20:37:52 +0200

    Reduce VS Code AgentHost noise during extension tests.

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,16 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+    files: 'out/test/**/*.test.js',
+
+    version: '1.110.0',
+
+    launchArgs: [
+        '--disable-extensions',
+        '--disable-telemetry',
+        '--disable-experiments',
+        '--skip-welcome',
+        '--skip-release-notes',
+        '--disable-workspace-trust',
+    ],
 });
